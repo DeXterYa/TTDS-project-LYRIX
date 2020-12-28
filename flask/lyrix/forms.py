1660ef6cf31782efe1aedb2daf7555ca76b83abe
Dexter Yang <dextery991@gmail.com>
2020-12-28 17:36:18 +0000

    advanced search

--- flask/lyrix/forms.py
+++ flask/lyrix/forms.py
@@ -4,4 +4,7 @@ from wtforms.validators import DataRequired
 
 class SearchBox(FlaskForm):
 	lyrics = StringField('Search Lyrics', validators=[DataRequired()])
+	singer = StringField('Singer')
+	year_begin = StringField('Begin')
+	year_end = StringField('End')
 	submit = SubmitField('Search')
