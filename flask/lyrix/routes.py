6c7e00ea512acd148015cde86b2856cabc8895a3
Dexter Yang <dextery991@gmail.com>
2020-12-28 15:42:10 +0000

    add bootstrap and a search bar

--- flask/lyrix/routes.py
+++ flask/lyrix/routes.py
@@ -1,12 +1,22 @@
-from flask import render_template
+from flask import render_template, url_for, flash, redirect
 from lyrix import app
+from lyrix.forms import SearchBox
 
 # different pages
-@app.route('/') # the root page of the website
-@app.route('/home')
+@app.route('/', methods=['GET', 'POST']) # the root page of the website
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+	form = SearchBox()
+	if form.validate_on_submit():
+		flash(f'Results found {form.lyrics.data}!', 'success')
+		return redirect(url_for('home'))
+	return render_template('home.html', form=form)
 
 @app.route('/about') # the root page of the website
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/test') # the root page of the website
+def test():
+    return render_template('test.html')
