237adf5ae315fa08f1b9dd87c1dd90334a0860d3
Dexter Yang <dextery991@gmail.com>
2020-12-29 20:48:16 +0000

    update MongoDB in README.md

--- flask/lyrix/models.py
+++ flask/lyrix/models.py
@@ -1,7 +1,9 @@
 from lyrix import db
 
 class Lyrics(db.Document):
+	# connect with a specific collection in the database
 	meta = {'collection':'lyrics_group1'}
+	# a list of fields
 	index = db.StringField()
 	artist = db.StringField()
 	song = db.StringField()
