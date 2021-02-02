d323b0c99fb704f668e43d026698d75301c6ccb1
Marti242 <Marti242@interia.pl>
2021-02-02 14:37:25 +0100

    Saves index in JSON file. On branch index Changes to be committed: 	modified:   index.py

--- index.py
+++ index.py
@@ -9,6 +9,7 @@ from collections      import OrderedDict
 from numpy            import argsort
 from numpy            import array
 from json             import loads
+from json             import dump
 from tqdm             import trange
 
 # Class for preprocessing text. Applies case folding, tokenization and stemming and
@@ -78,114 +79,55 @@ class Preprocessor:
         
         return txt
 
-# Class used to represent the inverted index for the single word. It has 3 fields:
-# - term         - the stem of a word for which the inverted index is built.
-# - documentFreq - number of documents in which the term occurs
-# - termFreqs    - dictionary in which each key is a document id. The value for a specific key 
-#                  is the term frequency in that document.
-class TermIndex:
-    
-    def __init__(self, term=''):
-        self.term         = term
-        self.documentFreq = 0
-        self.termFreqs    = {}
-        
-    # Converts string into a TermIndex object. Used to build an index from the already saved index. 
-    # Returns the resulting TermIndex object.
-    def fromString(self, idxTxt):
-        first             = idxTxt.index(':')
-        firstNewline      = idxTxt.index('\n')
-        self.term         = idxTxt[:first]
-        self.documentFreq = int(idxTxt[first+1:firstNewline])
-        idxTxt            = idxTxt[firstNewline+1:]
-        termFreqs         = idxTxt.split('\n')[:-1]
-        
-        for termFreq in termFreqs:
-            first                 = termFreq.index(':')
-            docNo                 = int(termFreq[1:first])
-            freq                  = int(termFreq[first+2:])
-            self.termFreqs[docNo] = freq
-            
-        return self
-        
-    # Converts the index into a string and returns it.
-    def toString(self):
-        indexString = self.term + ':' + str(self.documentFreq) + '\n'
-        
-        for docId, termFrequency in self.termFreqs.items():
-            indexString += '\t' + str(docId) + ': ' + str(termFrequency) + '\n'
-            
-        return indexString
-
-# Class used for building index and searching the text collection. Has 3 fields:
+# Class used for building index and searching the text collection. Has 2 fields:
 # - preprocessor - an instance of the Preprocessor class. Used to preprocess the text collection 
 #                  when index is built and for the preprocessing of queries.
-# - docNos       - list of document ids in the collection.
-# - index        - list of TermIndex objects.
+# - index        - Dictionary maps each term to a dictionary that maps a document ID to the
+#                  term frequency of that term in that document. To get document frequency just
+#                  compute len(index[term]). The total number of documents is stored under a special
+#                  name DOCUMENT_COUNT.
 class SearchEngine:
     
-    # Builds index. fileName - the name of a file from which the index should be built. indexReady 
-    # is boolean which indicates if the index was already built and saved in a file such 
-    # as 'index.txt'. To load the already saved index provide fileName that ends with .txt.
-    # In such case indexReady should be True. To build an index from scratch pass indexReady 
-    # as False and name of .json file as fileName.
-    def __init__(self, fileName, indexReady):
+    def __init__(self):
         self.preprocessor = Preprocessor()
-        
-        # Determines which function to use to build an index.
-        if indexReady:
-            builder = self.loadIndex
-        else:
-            builder = self.buildIndex
-        
-        # Populates docNos and index field of the object.
-        builder(fileName)
             
-    # Loads index from ready .txt file. Populates the docNos and index fields of the SearchEngine
-    # object. Assumes that inverted indexes for two terms in the file .txt file are separated 
-    # by a newline.
+    # Loads index from ready .json file. Populates the index field of the SearchEngine object.
     def loadIndex(self, fileName):
-        self.index  = []
-        self.docNos = set()
-    
         with open(fileName, 'r') as file:
-            idxTxt = ''
+            self.index = loads([line for line in file][0])
             
-            for line in file:
+            file.close()
 
-                # Assumes that inverted indexes for two terms in the file .txt file are separated 
-                # by a newline.
-                if line == '\n':
-                    termIndex   = TermIndex().fromString(idxTxt)
-                    idxTxt      = ''
-                    self.docNos = self.docNos.union(set(list(termIndex.termFreqs.keys())))
-                    
-                    self.index.append(termIndex)
-                else:
-                    idxTxt += line
-        
-        self.docNos = list(self.docNos)
-        
-        self.docNos.sort()
-
-    def loadJSON(self, fileName):
+    # Loads a dataset from .json file. The dataset should have a form of dictionary in which each
+    # song ID is mapped to the song object.
+    # Outputs:
+    #   - texts  - list of songs lyrics
+    #   - docNos - list with IDs of documents. texts[i] is the lyrics conentent of a song with 
+    #              docNos[i] ID.
+    # TO DO:
+    # Currently builds index only from lyrics field. Enable searching on other metadata too.
+    def loadDataset(self, fileName):
         with open(fileName, 'r') as file:
             songs = loads([line for line in file][0])
 
             file.close()
 
-        self.docNos = list(songs.keys())
-        texts       = [song['lyrics'] for song in songs.values()]
+        docNos = list(songs.keys())
+        texts  = [song['lyrics'] for song in songs.values()]
 
-        return texts
+        return texts, docNos
     
-    # Takes the name of json file - fileName and extract lyrics from such file. The json file should 
+    # Takes the name of .json file - inName and extract lyrics from such file. The json file should 
     # hold a dictionary of songs in which each song ID is mapped to a song object. The function 
-    # builds an index from such file and saves it in the index.txt file. Populates the index and 
-    # docNos fields of the SearchEngine object.
-    def buildIndex(self, fileName):
-        self.index = []
-        texts      = self.loadJSON(fileName)
+    # builds an index from such file and saves it in the outFile. Populates the index and field 
+    # of the SearchEngine object.
+    def buildIndex(self, inFile, outFile):
+        self.index    = dict()
+        texts, docNos = self.loadDataset(inFile)
+
+        # DOCUMENT_COUNT is a special entry in the index data structure that stores the total number
+        # of documents.
+        self.index['DOCUMENT_COUNT'] = len(docNos)
         
         # Preprocess each document.
         for txt_idx in trange(len(texts), desc='Preprocessing: '):
@@ -209,7 +151,9 @@ class SearchEngine:
         # Word at a time index building.
         for word_idx in trange(len(lexicon), desc='Index building: '):
             word      = lexicon[word_idx]
-            termIndex = TermIndex(word)
+
+            # Dictionary which maps each document ID to number of times a given term occurs in it.
+            termFreqs = dict()
 
             for idx, text in enumerate(texts):
 
@@ -217,35 +161,25 @@ class SearchEngine:
                 # if word is in the entire text of the document.
                 if word in sets[idx]:
 
-                    # Compute number of time word occurs in a document with docNos[idx] id.
-                    termFreq                              = sum([1 for term in text.split() if term == word])
-                    termIndex.termFreqs[self.docNos[idx]] = termFreq
+                    # Compute number of times word occurs in a document with docNos[idx] id.
+                    termFreq               = len([1 for term in text.split() if term == word])
+                    termFreqs[docNos[idx]] = termFreq
 
-                    # word occurred in another document so increment its frequency.
-                    termIndex.documentFreq = termIndex.documentFreq + 1
-
-            self.index.append(termIndex)
+            self.index[word] = termFreqs
         
-        # Saves the built index into index.txt file in the format specified in the coursework 
-        # document.
-        self.saveIndex()
+        # Saves the built index into outFile.
+        self.saveIndex(outFile)
     
-    # Saves inverted index into an index.txt file. Text representation of an inverted index for each 
-    # term is separated by a newline from the inverted index of another term.
-    def saveIndex(self):
-        indexTxt = ''
-
-        for termIndex in self.index:
-            indexTxt += termIndex.toString() + '\n'
-
-        with open('index.txt','w') as file:
-            file.write(indexTxt) 
+    # Saves inverted index into a file specified by outFile name. 
+    def saveIndex(self, outFile):
+        with open(outFile, 'w') as file:
+            dump(self.index, file)
             file.close()
     
     # Performs ranked search for the query string. Outputs list of most relevant documents.
     def rankedSearch(self, query):
 
-        # Preprocess the query
+        # Preprocess the query.
         query = self.preprocessor.preprocess(query)
 
         # Get dictionary in which each document id is mapped to its score for the query.
@@ -253,9 +187,9 @@ class SearchEngine:
 
         # Sort the documents by their scores in descending order. Ensure that if two documents have 
         # the same score the one with lower document id occurs first in the output.
-        scores       = OrderedDict(sorted(scores.items())[::-1])
-        idx          = list(argsort(list(scores.values()), kind='stable'))[::-1]
-        sortedDocs   = list(array(list(scores.keys()))[idx])
+        scores     = OrderedDict(sorted(scores.items())[::-1])
+        idx        = list(argsort(list(scores.values()), kind='stable'))[::-1]
+        sortedDocs = list(array(list(scores.keys()))[idx])
 
         return sortedDocs
     
@@ -263,16 +197,18 @@ class SearchEngine:
     # Result is returned in a dictionary in which each document id is mapped to its score for the
     # query with specified terms. The terms should result from preprocessing the original query.
     def computeTFIDFs(self, terms):
-        N      = len(self.docNos)
+        N      = self.index['DOCUMENT_COUNT']
         scores = {}
 
-        for termIndex in self.index:
-            if termIndex.term in terms:
-                idf = log10(N / termIndex.documentFreq)
+        for term, postings in self.index.items():
+            if term in terms:
+
+                # len(postings) is the document frequency of given term.
+                idf = log10(N / len(postings))
 
                 # Iterate over documents that contain the word.
-                for docId in termIndex.termFreqs:
-                    tf     = 1 + log10(termIndex.termFreqs[docId])
+                for docId, termFreq in postings.items():
+                    tf     = 1 + log10(termFreq)
                     weight = tf * idf
 
                     if docId not in scores:
@@ -281,5 +217,3 @@ class SearchEngine:
                     scores[docId] = scores[docId] + weight
 
         return scores
-
-    
